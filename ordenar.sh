ebf32a776f2f606019d79bfbbe03f428c5014060
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2017-08-08 20:55:10 -0300

    Add script for executing jar files

---
+++ ordenar.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#The name of the jar file thaat contains the algorithm
+algorithm=$1
+#The input for executing that algorithm
+input=$2
+
+## Permform some validation on input arguments, one example below
+if [ -z "$1" ] || [ -z "$2" ]; then
+        echo "Missing arguments, exiting.."
+        echo "Usage : $0 arg1 arg2"
+        exit 1
+fi
+
+java -jar $algorithm input
